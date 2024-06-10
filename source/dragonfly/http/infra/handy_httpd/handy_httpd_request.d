597222d1a64347ddc889b5cef8200d82b0e24d7f
Daniel Osorio O <41162715+dogia@users.noreply.github.com>
2024-06-09 19:57:32 -0500

    Create handy_httpd_request.d

---
+++ source/dragonfly/http/infra/handy_httpd/handy_httpd_request.d
@@ -0,0 +1,26 @@
+module dragonfly.http.infra.handy_httpd.handy_httpd_request;
+
+
+import source.dragonfly.http.core.http_request;
+import source.dragonfly.http.core.http_method;
+import handy_httpd : HttpRequestContext;
+
+class HandyHttpdRequest : HttpRequest {
+    private HTTP_METHOD _http_method = HTTP_METHOD.GET;
+
+    this(HttpRequestContext ctx) {
+        this.readMethod(ctx);
+    }
+
+    HTTP_METHOD method() {
+
+        return HTTP_METHOD.GET;
+    }
+
+    private void readMethod(HttpRequestContext ctx) {
+        // switch(ctx.request.method) {
+        //     case Method.GET:
+        //         break;
+        // }
+    }
+}
